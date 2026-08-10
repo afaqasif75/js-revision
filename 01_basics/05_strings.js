921dd10a06dc211dc7e275ed62c2e7b26f3f0b9f
Afaq Asif <afaqasif75@gmail.com>
2026-08-10 20:03:17 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "afaq"
+const repoCount = 3
+
+// console.log(name + repoCount + " Value");
+
+console.log(`My name is ${name.toUpp} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('afaqfc')
+//Key-Value Pairs
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
