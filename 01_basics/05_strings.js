06f38ca2d4b3fe588baa4e8a61917583956050c6
Afaq Asif <afaqasif75@gmail.com>
2026-08-13 19:58:31 +0000

    nums and maths

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,11 +3,35 @@ const repoCount = 3
 
 // console.log(name + repoCount + " Value");
 
-console.log(`My name is ${name.toUpp} and my repo count is ${repoCount}`);
+// console.log(`My name is ${name.toUpp} and my repo count is ${repoCount}`);
 
 
-const gameName = new String('afaqfc')
+const gameName = new String('afaq-aa-com')
 //Key-Value Pairs
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('q'));
+
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-7,-2)
+// console.log(anotherString);
+
+const newStringOne = "   afaq   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://afaq.com/afaq%20asif"
+
+// console.log(url.replace('%20', "-"));
+// console.log(url.includes('afaq'));
+
+console.log(gameName.split('-'));
+
