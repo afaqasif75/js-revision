1a0202245a5cf3852c9ca04f15af4285cf432292
Afaq Asif <afaqasif75@gmail.com>
2026-08-15 20:25:23 +0000

    data and time

--- 01_basics/07_date_time.js
+++ 01_basics/07_date_time.js
@@ -7,3 +7,23 @@ let myDate = new Date();
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
+let myCreateDate = new Date (2001, 6, 5, 2, 0, 12)
+// console.log(myCreateDate.toLocaleString());
+
+let anotherDate = new Date ("2003-12-07");
+// console.log(anotherDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+console.log((Math.floor(Date.now()/1000)));
+
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
+
+
