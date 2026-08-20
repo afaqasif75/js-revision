74c101b7b3e71922b68e0c11b665e331fb867350
Afaq Asif <afaqasif75@gmail.com>
2026-08-20 17:12:06 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,13 +22,9 @@ const regularUSer =  {
 
 // console.log(regularUSer.fullname?.userfullname.firstname);
 
- const obj1 = {
-    1: "a",
-    2: "b"
- }
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b" }
+const obj3 = {5: "a", 6: "b" }
 
- const obj2 = {
-    3: "a",
-    4: "b"
- }
-
+const obj4 = Object.assign({}, obj1, obj2, obj3)
+console.log(obj4);
