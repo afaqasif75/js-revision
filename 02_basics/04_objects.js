df28e826f7ed19273302312baf4326369f49c048
Afaq Asif <afaqasif75@gmail.com>
2026-08-21 20:24:13 +0000

    objects hasOwnProperty

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,5 +26,35 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b" }
 const obj3 = {5: "a", 6: "b" }
 
-const obj4 = Object.assign({}, obj1, obj2, obj3)
-console.log(obj4);
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+// console.log(obj4);
+
+const obj4 = {...obj1, ...obj2, ...obj3}
+// console.log(obj4);
+
+
+const users = [
+    {
+        id:1,
+        email: "a@ga.com"
+    },
+    {
+        id:2,
+        email: "b@ga.com"
+    },
+    {
+        id:3,
+        email: "c@ga.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // true
+console.log(tinderUser.hasOwnProperty('id')); // true
+console.log(tinderUser.hasOwnProperty('email')); // false
